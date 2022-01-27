87c71527d2a7ed090e953df33a43b8c73ca3b7d6
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-01-27 23:02:10 +0900

    project setup

---
+++ convenience-client/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="justify-center flex bg-yellow-300 items-center h-screen">
+    <div class="text-4xl">{{ msg }} 👋🏼</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+});
+</script>
