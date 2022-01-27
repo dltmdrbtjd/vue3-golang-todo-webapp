28a22ee5e57e0e5b831186f22588603c64282a4e
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-01-28 00:13:53 +0900

    cors set, header set

---
+++ convenience-server/pkg/middleware/cors.go
@@ -0,0 +1,19 @@
+package middleware
+
+import "github.com/gin-gonic/gin"
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization, Origin")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithError(204, gin.Error{})
+			return
+		}
+
+		c.Next()
+	}
+}
