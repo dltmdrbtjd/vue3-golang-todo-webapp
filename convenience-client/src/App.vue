40c560ee298c531a3585b4493cbf36e2a9149fd6
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-01 18:50:57 +0900

    display setting test

--- convenience-client/src/App.vue
+++ convenience-client/src/App.vue
@@ -3,13 +3,4 @@
   <h1 class="text-3xl font-bold underline text-white mt-4">Hello world!</h1>
 </template>
 <script setup lang="ts"></script>
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- convenience-client/src/components/HelloWorld.vue
+++ convenience-client/src/components/HelloWorld.vue
@@ -1,12 +1,14 @@
 <template>
-  <button
-    class="bg-sky-600 text-stone-50 px-2 py-1 rounded-md"
-    @click="store.Increse(1)"
-  >
-    숫자 더하기
-  </button>
-  <p class="text-white">state: {{ store.testNumber }}</p>
-  <h2 class="text-white">getters: {{ store.getCurrentNumber }}</h2>
+  <div class="mx-auto max-w-xl w-full flex justify-between">
+    <button
+      class="bg-sky-600 text-stone-50 px-2 py-1 rounded-md"
+      @click="store.Increse(1)"
+    >
+      숫자 더하기
+    </button>
+    <p class="text-white">state: {{ store.testNumber }}</p>
+    <h2 class="text-white">getters: {{ store.getCurrentNumber }}</h2>
+  </div>
 </template>
 <script setup lang="ts">
 import { useTestStore } from "@/store/test";
@@ -14,20 +16,4 @@ import { useTestStore } from "@/store/test";
 const store = useTestStore();
 </script>
 
-<style scoped>
-a {
-  color: #42b983;
-}
-
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-}
-</style>
+<style scoped></style>
