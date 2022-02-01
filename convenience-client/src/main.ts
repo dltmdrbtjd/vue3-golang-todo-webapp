46f0851cf17ec6b335f96bdee0b3eec9bff30cfe
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-01 18:46:11 +0900

    default css setup

--- convenience-client/src/main.ts
+++ convenience-client/src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from "vue";
 import App from "@/App.vue";
 import router from "@/routes";
 import { createPinia } from "pinia";
+import "./styles/tailwind.scss";
+import "./styles/main.scss";
 
 createApp(App).use(createPinia()).use(router).mount("#app");
