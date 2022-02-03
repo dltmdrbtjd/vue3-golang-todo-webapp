4f801479f7fa1b933e20a1ce5189a4d486cf8f53
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-03 22:01:39 +0900

    edit todo struct, create updateTodo,TodoStatus struct

--- convenience-server/pkg/models/todo_model.go
+++ convenience-server/pkg/models/todo_model.go
@@ -1,11 +1,18 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
 type Todo struct {
-	Id       primitive.ObjectID `json:"id,omitempty"`
-	Title    string             `json:"title,omitempty" validate:"required"`
-	Content  string             `json:"content,omitempty" validate:"required"`
-	Status   string             `json:"status,omitempty"`
-	UserName string             `json:"username,omitempty" validate:"required"`
+	Title    string `json:"title,omitempty" binding:"required"`
+	Content  string `json:"content,omitempty" binding:"required"`
+	Status   bool   `json:"status,omitempty"`
+	UserName string `json:"username,omitempty" binding:"required"`
+}
+
+type UpdateTodo struct {
+	Title    string `json:"title"`
+	Content  string `json:"content"`
+	UserName string `json:"username"`
+}
+
+type TodoStatus struct {
+	Status bool `json:"status,omitempty"`
 }
