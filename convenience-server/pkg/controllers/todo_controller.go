6b6f0ac77aad1e770e77c9325c25f70ebd822928
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-03 18:54:40 +0900

    create delete user todo item

--- convenience-server/pkg/controllers/todo_controller.go
+++ convenience-server/pkg/controllers/todo_controller.go
@@ -45,3 +45,25 @@ func GetTodoList(c *gin.Context) {
 	}
 	c.JSON(http.StatusCreated, response.TodoResponse{Status: http.StatusCreated, Message: "success", Data: map[string]interface{}{"data": todoList}})
 }
+
+func DeleteTodo(c *gin.Context) {
+	var userName string
+
+	if err := c.BindJSON(&userName); err != nil {
+		c.JSON(http.StatusBadRequest, response.TodoResponse{Status: http.StatusBadRequest, Message: "error", Data: map[string]interface{}{"data": err.Error()}})
+		return
+	}
+
+	if validationErr := validate.Struct(&userName); validationErr != nil {
+		c.JSON(http.StatusBadRequest, response.TodoResponse{Status: http.StatusBadRequest, Message: "error", Data: map[string]interface{}{"data": validationErr.Error()}})
+		return
+	}
+
+	err := services.DeleteTodoService(userName)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, response.TodoResponse{Status: http.StatusInternalServerError, Message: "error", Data: map[string]interface{}{"data": err.Error()}})
+		return
+	}
+
+	c.JSON(http.StatusCreated, response.TodoResponse{Status: http.StatusCreated, Message: "success", Data: map[string]interface{}{}})
+}
--- convenience-server/pkg/services/todo_service.go
+++ convenience-server/pkg/services/todo_service.go
@@ -35,14 +35,14 @@ func CreateTodoService(todo models.Todo) (models.Todo, error) {
 	return newTodo, err
 }
 
-func GetTodoListService(username string) ([]*models.Todo, error) {
+func GetTodoListService(userName string) ([]*models.Todo, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	findOptions := options.Find()
 	var todoList []*models.Todo
 
-	cur, err := todoCollection.Find(ctx, bson.M{"username": username}, findOptions)
+	cur, err := todoCollection.Find(ctx, bson.M{"username": userName}, findOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -63,4 +63,20 @@ func GetTodoListService(username string) ([]*models.Todo, error) {
 
 	cur.Close(ctx)
 	return todoList, err
+}
+
+func DeleteTodoService(objId string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	result, err := todoCollection.DeleteOne(ctx, bson.M{"id": objId})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if result.DeletedCount < 1 {
+		log.Fatal("User todo specified Id not found")
+	}
+
+	return err
 }
