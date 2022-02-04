80b9f53a8ddc08ccb7c9f5e3be6a8094b05d0a8a
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-04 22:00:43 +0900

    add to graphql handler

--- convenience-server/cmd/main.go
+++ convenience-server/cmd/main.go
@@ -6,7 +6,9 @@ import (
 	"github.com/Convenience-Tools/convenience-server/configs"
 	"github.com/Convenience-Tools/convenience-server/pkg/middleware"
 	"github.com/Convenience-Tools/convenience-server/pkg/routes"
+	"github.com/Convenience-Tools/convenience-server/pkg/schema"
 	"github.com/gin-gonic/gin"
+	"github.com/graph-gophers/graphql-go/relay"
 )
 
 func main() {
@@ -17,8 +19,19 @@ func main() {
 
 	configs.ConnectDB()
 	routes.TodoRoutes(r)
+	r.POST("/query", GraphQLHandler())
 
 	if err := r.Run(":8081"); err != nil {
 		fmt.Printf("failed server start, err%v\n", err)
+	}
+}
+
+func GraphQLHandler() gin.HandlerFunc {
+	h := relay.Handler{
+		Schema: schema.Schema,
+	}
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
 	}
 }
