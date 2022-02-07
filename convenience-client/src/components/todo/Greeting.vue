7fe65c4799fcf13d268492da21527d13f2e2defd
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-08 01:08:19 +0900

    style: div,p margin top change

--- convenience-client/src/components/todo/Greeting.vue
+++ convenience-client/src/components/todo/Greeting.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-center">
+  <div class="text-center mt-10">
     <p>안녕하세요, dltmdrbtjd님</p>
     <p>2022년 02월 07일 입니다.</p>
     <p>오늘도 좋은 하루 보내세요🙂</p>
@@ -8,6 +8,6 @@
 <script setup lang="ts"></script>
 <style scoped lang="scss">
 p {
-  @apply text-white font-semibold text-2xl mt-2;
+  @apply text-white font-semibold text-2xl mt-4;
 }
 </style>
