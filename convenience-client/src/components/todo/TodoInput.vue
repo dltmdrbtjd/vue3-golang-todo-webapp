4e63cd86444bc4612291c33288051cc5a976ec57
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-08 01:07:45 +0900

    add v-model connect props value

--- convenience-client/src/components/todo/TodoInput.vue
+++ convenience-client/src/components/todo/TodoInput.vue
@@ -1,4 +1,39 @@
 <template>
-  <div></div>
+  <div class="flex justify-center mt-10 w-full">
+    <input
+      class="input"
+      type="text"
+      :type="type"
+      :placeholder="placeholder"
+      v-model="value"
+    />
+  </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useVModel } from "@vueuse/core";
+
+const props = withDefaults(
+  defineProps<{
+    label?: string;
+    type?: string;
+    modelValue?: string | number;
+    placeholder?: string;
+  }>(),
+  {
+    label: "",
+    type: "text",
+    modelValue: "",
+    placeholder: "",
+  }
+);
+
+const emit = defineEmits(["update:modelValue"]);
+const value = useVModel(props, "modelValue", emit);
+
+console.log(props.modelValue);
+</script>
+<style scoped lang="scss">
+.input {
+  @apply border rounded py-2 px-2 text-xl font-bold text-gray-700 dark:text-white dark:bg-sky-800 leading-tight focus:outline-none transition-colors w-1/2;
+}
+</style>
--- convenience-client/src/pages/Todo.vue
+++ convenience-client/src/pages/Todo.vue
@@ -1,6 +1,18 @@
 <template>
   <Greeting />
+  <TodoInput
+    :placeholder="'TODO를 작성해주세요'"
+    v-model="text"
+    @keydown.enter.prevent="save"
+  />
 </template>
 <script setup lang="ts">
 import Greeting from "@/components/todo/Greeting.vue";
+import TodoInput from "@/components/todo/TodoInput.vue";
+import { ref } from "vue";
+
+const text = ref<string>("");
+const save = () => {
+  console.log(text.value);
+};
 </script>
