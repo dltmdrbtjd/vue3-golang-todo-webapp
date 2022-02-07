73cb0949457d6007f319668699d55ea6f6f30619
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-07 20:55:26 +0900

    page default style setup

--- convenience-client/src/App.vue
+++ convenience-client/src/App.vue
@@ -1,6 +1,12 @@
 <template>
-  <router-view />
-  <h1 class="text-3xl font-bold underline text-white mt-4">Hello world!</h1>
+  <div class="relative w-full h-full">
+    <AppBar />
+    <main class="mx-auto max-w-3xl w-full h-full">
+      <router-view />
+    </main>
+  </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppBar from "@/components/common/AppBar.vue";
+</script>
 <style></style>
