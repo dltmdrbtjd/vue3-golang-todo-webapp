69db157ada3a6b837bc8d3ceabb8ac5d5b91f454
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-09 23:56:09 +0900

    todo store setup

---
+++ convenience-client/src/store/todo/type.ts
@@ -0,0 +1,14 @@
+export interface Todo {
+  id?: string;
+  title: string;
+  status?: boolean;
+  content: string;
+  username: string;
+  createAt?: Date;
+}
+
+// Title:    todo.Title,
+// Status:   false,
+// Content:  todo.Content,
+// UserName: todo.UserName,
+// CreateAt: timeFormat,
