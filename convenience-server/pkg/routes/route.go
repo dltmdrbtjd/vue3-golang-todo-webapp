3c7e7b4881c2cb2e24eee99b3b0b834b757b3123
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-11 00:10:10 +0900

    refac: routes

---
+++ convenience-server/pkg/routes/route.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/Convenience-Tools/convenience-server/pkg/routes/todo"
+	"github.com/gin-gonic/gin"
+)
+
+func Register(r *gin.Engine) {
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"response": "pong",
+		})
+	})
+	rootGroup := r.Group("/")
+	todo.Register(rootGroup, nil)
+}
---
+++ convenience-server/pkg/routes/todo/register.go
@@ -0,0 +1,17 @@
+package todo
+
+import (
+	"github.com/Convenience-Tools/convenience-server/pkg/controllers/todo"
+	"github.com/gin-gonic/gin"
+)
+
+func Register(parentGroup *gin.RouterGroup, controller todo.Controller) {
+	if controller == nil {
+		controller = todo.CreateController()
+	}
+	todoGroup := parentGroup.Group("/todo")
+	todoGroup.Use()
+	{
+		todoGroup.POST("", controller.CreateTodoItem)
+	}
+}
