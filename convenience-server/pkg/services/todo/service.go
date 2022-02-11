7d3af0f145a09af3c326cad7d340459f743ab3d7
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-11 21:30:47 +0900

    refac: todo service

--- convenience-server/pkg/services/todo/service.go
+++ convenience-server/pkg/services/todo/service.go
@@ -1,9 +1,16 @@
 package todo
 
-import "github.com/Convenience-Tools/convenience-server/pkg/repositories/todo"
+import (
+	todoModel "github.com/Convenience-Tools/convenience-server/pkg/models/todo"
+	"github.com/Convenience-Tools/convenience-server/pkg/repositories/todo"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Service interface {
-	CreateTodo(title string, status string, content string, username string) error
+	CreateTodo(title string, content string, username string) error
+	GetTodoList(username string) ([]todoModel.Todo, error)
+	DeleteTodoItem(todoId primitive.ObjectID) error
+	EditTodoItem(todoId primitive.ObjectID, content string, title string) error
 }
 
 type service struct {
--- convenience-server/pkg/services/todo/todo.go
+++ convenience-server/pkg/services/todo/todo.go
@@ -3,18 +3,57 @@ package todo
 import (
 	"context"
 
+	"github.com/Convenience-Tools/convenience-server/pkg/models/todo"
 	"github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-func (s *service) CreateTodo(title string, status string, content string, username string) error {
+func (s *service) CreateTodo(title string, content string, username string) error {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	err := s.todoRepository.CreateTodo(ctx, title, status, content, username)
+	err := s.todoRepository.CreateTodo(ctx, title, content, username)
 	if err != nil {
-		logrus.Errorln("CreateTodo error: ", err.Error())
+		logrus.Errorln("create todo error: ", err.Error())
 		return err
 	}
 
 	return nil
+}
+
+func (s *service) GetTodoList(username string) ([]todo.Todo, error) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	todoList, err := s.todoRepository.GetTodoList(ctx, username)
+	if err != nil {
+		logrus.Errorln("get todolist error: ", err.Error())
+		return nil, err
+	}
+
+	return todoList, err
+}
+
+func (s *service) DeleteTodoItem(todoId primitive.ObjectID) error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := s.todoRepository.DeleteTodoItem(ctx, todoId)
+	if err != nil {
+		logrus.Errorln("delete todo item error:", err.Error())
+	}
+
+	return err
+}
+
+func (s *service) EditTodoItem(todoId primitive.ObjectID, content string, title string) error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := s.todoRepository.EditTodoItem(ctx, todoId, content, title)
+	if err != nil {
+		logrus.Errorln("edit todo item error:", err.Error())
+	}
+
+	return err
 }
