0276979cd9cff986e4632e708f20be2a292b875f
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-13 01:09:20 +0900

    create todo card component

---
+++ convenience-client/src/components/todo/TodoCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="mt-10 card border w-2/3 mx-auto bg-gray-800 text-white">
+        <div>
+            <strong class="font-bold text-2xl mt-2 block">오늘 todo카드 까지 완료하기</strong>
+        </div>
+        <div class="flex justify-end w-full">
+            <div class="flex">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="#0ea5e9">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                </svg>
+                <svg v-if="status" xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="green">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                </svg>
+                <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="red">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+let status = true
+</script>
+<style scoped lang="scss">
+svg {
+    @apply ml-2 cursor-pointer
+}
+</style>
--- convenience-client/src/pages/Todo.vue
+++ convenience-client/src/pages/Todo.vue
@@ -5,10 +5,12 @@
     v-model="text"
     @keydown.enter.prevent="save"
   />
+  <TodoCard />
 </template>
 <script setup lang="ts">
 import Greeting from "@/components/todo/Greeting.vue";
 import TodoInput from "@/components/todo/TodoInput.vue";
+import TodoCard from "@/components/todo/TodoCard.vue";
 import { ref } from "vue";
 
 const text = ref<string>("");
