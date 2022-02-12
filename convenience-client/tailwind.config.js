e343c637095fdc746ce4f9248fecb2d32af37e82
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-13 01:09:03 +0900

    pkg update

--- convenience-client/tailwind.config.js
+++ convenience-client/tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  // mode: 'jit',
+  // purge if want used tailwind version up 3.0
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx,scss}'
+  ],
   theme: {
     extend: {},
   },
