2c608becf13434652e0c156f2aa70b3b5aa7de53
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-14 23:54:50 +0900

    import axios plugins

--- convenience-client/src/main.ts
+++ convenience-client/src/main.ts
@@ -2,7 +2,8 @@ import { createApp } from "vue";
 import App from "@/App.vue";
 import router from "@/routes";
 import { createPinia } from "pinia";
-import "./styles/tailwind.scss";
-import "./styles/main.scss";
+import "@/styles/tailwind.scss";
+import "@/styles/main.scss";
+import "@/plugins/axios"
 
 createApp(App).use(createPinia()).use(router).mount("#app");
