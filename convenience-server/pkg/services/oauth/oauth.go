04f60648d9db287a79b54290ff38cf93d9cf0abb
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-20 04:08:59 +0900

    change url state

--- convenience-server/pkg/services/oauth/oauth.go
+++ convenience-server/pkg/services/oauth/oauth.go
@@ -2,6 +2,8 @@ package oauth
 
 import (
 	"context"
+	"crypto/rand"
+	"encoding/base64"
 	"os"
 
 	"github.com/sirupsen/logrus"
@@ -11,17 +13,23 @@ import (
 
 var (
 	googleOauthConfig = oauth2.Config{
-		RedirectURL:  "http://localhost:8081/google-login/callback",
+		RedirectURL:  "http://localhost:8080/google-login/callback",
 		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
 		ClientSecret: os.Getenv("GOOGLE_SECRET_KEY"),
-		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
+		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"},
 		Endpoint:     google.Endpoint,
 	}
-	RandomState = "random"
 )
 
+func GetToken() string {
+	b := make([]byte, 32)
+	rand.Read(b)
+	return base64.RawStdEncoding.EncodeToString(b)
+}
+
 func (s *service) GoogleLogin() string {
-	uri := googleOauthConfig.AuthCodeURL(RandomState)
+	urlToken := GetToken()
+	uri := googleOauthConfig.AuthCodeURL(urlToken)
 	if uri == "" {
 		logrus.Errorln("not found google oauth uri!")
 		return ""
