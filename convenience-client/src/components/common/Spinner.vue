19de8d8c52285e3be945d22e38d14af2b166ea3e
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-20 04:08:37 +0900

    create common spinner component

---
+++ convenience-client/src/components/common/Spinner.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex justify-center items-center">
+        <div class="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+
+</script>
