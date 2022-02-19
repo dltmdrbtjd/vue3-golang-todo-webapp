ec2694335d6ad3dd592764a0690263c202e37721
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-20 04:08:28 +0900

    google oauth login loading page

---
+++ convenience-client/src/pages/GoogleAuth.vue
@@ -0,0 +1,16 @@
+<template>
+    <Spinner />
+</template>
+<script setup lang="ts">
+import { onMounted } from "vue";
+import Spinner from '@/components/common/Spinner.vue';
+import { useRoute } from "vue-router";
+import $http from "axios";
+
+const route = useRoute();
+let authQuery = route.fullPath.split("?")[1];
+onMounted(async () => {
+    const resp = await $http.get(`/google-login/callback?${authQuery}`)
+    console.log(resp.data)
+})
+</script>
