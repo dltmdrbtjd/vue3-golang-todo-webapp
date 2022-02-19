1db661ee37b519c9a3a3e52d49b3323d3fc90cb1
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-19 23:18:14 +0900

    google oauth client test

--- convenience-client/src/App.vue
+++ convenience-client/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="relative w-full h-full">
     <AppBar />
+    <Login />
     <main class="mx-auto max-w-3xl w-full h-full">
       <router-view />
     </main>
@@ -8,5 +9,6 @@
 </template>
 <script setup lang="ts">
 import AppBar from "@/components/common/AppBar.vue";
+import Login from "./components/login/Login.vue";
 </script>
 <style></style>
