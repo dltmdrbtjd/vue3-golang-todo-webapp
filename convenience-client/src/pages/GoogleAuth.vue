c04fe6268836101960aff85a0c1fa18d82b86102
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-20 22:02:11 +0900

    refac: Google OAuth redirect url Page Component

--- convenience-client/src/pages/GoogleAuth.vue
+++ convenience-client/src/pages/GoogleAuth.vue
@@ -6,11 +6,23 @@ import { onMounted } from "vue";
 import Spinner from '@/components/common/Spinner.vue';
 import { useRoute } from "vue-router";
 import $http from "axios";
+import router from "@/routes";
+import { setLocalStorage } from "@/scripts/localStorage";
+import { useUserStore } from "@/store/user";
 
 const route = useRoute();
 let authQuery = route.fullPath.split("?")[1];
+
+const userStore = useUserStore();
+function getUserInfoDetail() {
+  userStore.getUserInfo()
+}
+
 onMounted(async () => {
     const resp = await $http.get(`/google-login/callback?${authQuery}`)
-    console.log(resp.data)
+    setLocalStorage("convenience-tools-email", resp.data.data)
+    getUserInfoDetail()
+    userStore.googleTokenVerification(true)
+    router.push("/")
 })
 </script>
