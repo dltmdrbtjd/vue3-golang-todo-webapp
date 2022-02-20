da199e5d18ee4504c542ec1700216fb93a238dc3
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-20 21:59:29 +0900

    create save user,get user, token verification repo

---
+++ convenience-server/pkg/repositories/user/repository.go
@@ -0,0 +1,36 @@
+package user
+
+import (
+	"context"
+
+	"github.com/Convenience-Tools/convenience-server/pkg/db"
+	"github.com/Convenience-Tools/convenience-server/pkg/models/user"
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/oauth2"
+)
+
+const (
+	userCollectionName = "user"
+)
+
+type Repository interface {
+	SaveGoogleUserInfo(ctx context.Context, userInfo *user.GoogleUserInfo) error
+	GetGoogleUserInfo(ctx context.Context, userEmail string) (user.UserInfo, error)
+	GoogleTokenVerification(ctx context.Context, userEmail string) (*oauth2.Token, error)
+}
+
+type repository struct {
+	userCollection *mongo.Collection
+}
+
+func NewRepository(userCollection *mongo.Collection) Repository {
+	return &repository{
+		userCollection: userCollection,
+	}
+}
+
+func CreateRepository() Repository {
+	return NewRepository(
+		db.GetDB().Collection(userCollectionName),
+	)
+}
