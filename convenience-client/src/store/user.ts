d1b688efdeb965886a762b3063b8719b00b839c2
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-20 22:00:51 +0900

    create user Store for get userinfo,verification

---
+++ convenience-client/src/store/user.ts
@@ -0,0 +1,38 @@
+import { User } from '@/models/user.model';
+import { getLocalStorage } from '@/scripts/localStorage';
+import { defineStore } from 'pinia';
+import $http from 'axios';
+
+interface State {
+    userInfo: User
+    oauthVerification: boolean
+}
+
+export const useUserStore = defineStore("user", {
+    state: (): State => ({
+        userInfo: {
+            name: "",
+            picture: ""
+        },
+        oauthVerification: false
+    }),
+    actions: {
+        async getUserInfo() {
+            const userEmail = getLocalStorage("convenience-tools-email");
+            try {
+                const resp = await $http.get(`/google-userinfo/${userEmail}`)
+                this.userInfo = resp.data.data
+                console.log(resp)
+            } catch(error) {
+                console.error(error)
+            }
+        },
+        googleTokenVerification(status: boolean) {
+            this.oauthVerification = status
+        }
+    },
+    getters: {
+        getUserDetail: (state): User => state.userInfo,
+        getTokenVerification: (state): boolean => state.oauthVerification,
+    }
+})
