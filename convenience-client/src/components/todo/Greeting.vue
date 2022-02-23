00729d4eb9f94e3b80f092b20ba87381b9f2c0c4
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-23 22:15:40 +0900

    change default username to real username

--- convenience-client/src/components/todo/Greeting.vue
+++ convenience-client/src/components/todo/Greeting.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="text-center mt-10">
-    <p>안녕하세요, dltmdrbtjd님</p>
+    <p>안녕하세요, {{ userInfo.name }}님</p>
     <p>2022년 02월 07일 입니다.</p>
     <p>오늘도 좋은 하루 보내세요🙂</p>
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useUserStore } from '@/store/user';
+import { computed } from 'vue';
+import { User } from '@/models/user.model';
+
+
+const userStore = useUserStore();
+const userInfo = computed<User>(() => userStore.getUserDetail);
+</script>
 <style scoped lang="scss">
 p {
   @apply text-white font-semibold text-2xl mt-4;
