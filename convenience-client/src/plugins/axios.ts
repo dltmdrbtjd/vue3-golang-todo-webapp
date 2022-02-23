e8b53434064ca4044d31eda2becb219cbb877d86
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-23 22:13:35 +0900

    if response had name save localStorage user name

--- convenience-client/src/plugins/axios.ts
+++ convenience-client/src/plugins/axios.ts
@@ -1,20 +1,24 @@
 import axios from "axios";
 import { App } from "vue";
 import getEnv from "@/scripts/env";
+import { setLocalStorage } from "@/scripts/localStorage";
 
 axios.defaults.baseURL = getEnv("VITE_APP_BASE_URL");
 
+axios.interceptors.response.use(
+    (response) => {
+        if(response.data.data?.name) {
+            setLocalStorage("username" , response.data.data?.name)
+        }
+        return Promise.resolve(response)
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
+
 export default {
     install: (app: App) => {
-        axios.interceptors.request.use(
-            (response) => {
-                return Promise.resolve(response)
-            },
-            (error) => {
-                return Promise.reject(error)
-            }
-        )
-        
         app.config.globalProperties.$http = axios;
     }
 }
