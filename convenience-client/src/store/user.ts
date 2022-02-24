d062718633947e5798132a30798a268d1de9a326
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-02-24 22:28:33 +0900

    chore: formatting

--- convenience-client/src/store/user.ts
+++ convenience-client/src/store/user.ts
@@ -4,34 +4,34 @@ import { defineStore } from 'pinia';
 import $http from 'axios';
 
 interface State {
-    userInfo: User
-    oauthVerification: boolean
+  userInfo: User;
+  oauthVerification: boolean;
 }
 
-export const useUserStore = defineStore("user", {
-    state: (): State => ({
-        userInfo: {
-            name: "",
-            picture: ""
-        },
-        oauthVerification: false
-    }),
-    actions: {
-        async getUserInfo() {
-            const googleAccessToken = getLocalStorage("google-access-token");
-            try {
-                const resp = await $http.get(`/google-userinfo/${googleAccessToken}`)
-                this.userInfo = resp.data.data
-            } catch(error) {
-                console.error(error)
-            }
-        },
-        googleTokenVerification(status: boolean) {
-            this.oauthVerification = status
-        }
+export const useUserStore = defineStore('user', {
+  state: (): State => ({
+    userInfo: {
+      name: '',
+      picture: '',
     },
-    getters: {
-        getUserDetail: (state): User => state.userInfo,
-        getTokenVerification: (state): boolean => state.oauthVerification,
-    }
-})
+    oauthVerification: false,
+  }),
+  actions: {
+    async getUserInfo() {
+      const googleAccessToken = getLocalStorage('google-access-token');
+      try {
+        const resp = await $http.get(`/google-userinfo/${googleAccessToken}`);
+        this.userInfo = resp.data.data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    googleTokenVerification(status: boolean) {
+      this.oauthVerification = status;
+    },
+  },
+  getters: {
+    getUserDetail: (state): User => state.userInfo,
+    getTokenVerification: (state): boolean => state.oauthVerification,
+  },
+});
